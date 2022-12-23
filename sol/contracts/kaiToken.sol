762a4d0a51577090ffa790e394403c97df9565cd
AnJuYeong <dksenfn7907@naver.com>
2022-12-23 16:03:35 +0900

    20221223 토큰 구매 판매 함수 구현완료

--- sol/contracts/kaiToken.sol
+++ sol/contracts/kaiToken.sol
@@ -45,6 +45,7 @@ contract KaiToken is ERC20, Ownable {
         }
     }
 
+    // 토큰 판매 함수
    function sellToken(uint256 _amount) public payable{
         require(balanceOf(msg.sender) >= _amount, "you fucking no Token ok?");
         uint256 etherAmount = _amount/rate;
@@ -52,28 +53,15 @@ contract KaiToken is ERC20, Ownable {
         tokenTransferSell(owner(),msg.sender, _amount);
         payable(msg.sender).transfer(etherAmount);
     }
+    //ERC20에 밑에 두개 함수 추가 하기
+    // token 구매
+    // function tokenTransferBuy(address owner, address to, uint256 amount) public virtual {
+    //     _balances[owner] -= amount;
+    //     _balances[to] += amount;
+    // }
+    // token 판매
+    // function tokenTransferSell(address owner, address to, uint256 amount) public virtual {
+    //     _balances[owner] += amount;
+    //     _balances[to] -= amount;
+    // }
 }
-
-// (0) 0x5e5e6f059CE704Ab186402c0fd3153BE76FA00F6 (100 ETH)
-// (1) 0x99CFAA1E63132C22A45b251EC5F925B3770792FE (100 ETH)
-// (2) 0xdD2A5c73eC4C73d3226DafA805031f0f174ab0c3 (100 ETH)
-// (3) 0x00e10cdce56d5EF25428Bb7cFF1451cB72d70d29 (100 ETH)
-// (4) 0xfD82a9508287dd65f67E6752ee97cc13826367aA (100 ETH)
-// (5) 0xa4f7d58dC15d0f2273bE760c7778A65e2E9F6f00 (100 ETH)
-// (6) 0x1C8D32ADdB66fD8a4740aFD63D6980659F7a5A85 (100 ETH)
-// (7) 0xfed1315BEFFb6A32820E2930FBaA839d59De6Ba0 (100 ETH)
-// (8) 0x105a7099325d7609aEf74D0bfEb3ebd5B7D78259 (100 ETH)
-// (9) 0x309c0A6Efb1ba978404949A3A8934cc09e83c604 (100 ETH)
-
-// Private Keys
-// ==================
-// (0) 0xd478d3486055b35bf71aa3cebc7af59b6df240674b839a694a180c3706cfab12
-// (1) 0x5c579894b6ea0f648729d631bf6789c79162eddcec56bfe05ef45ad01f77070f
-// (2) 0x142a66182dabc3d600f55fe20ed25878073cbe3e563b4e4062961439286b1cc3
-// (3) 0x2c8204122ad044fb422a544486a0b3fa14675fc4ee355d3e99c8009c4cd3bc2a
-// (4) 0xdec6b98e64ee1167ceea5ea413f45b7aa9a4478f6f8bf343ee6a13ebfaed69f6
-// (5) 0x6ccb4d2aeb1d0a625db8ee9f6f76077f4c99af449e7d4a101e9051b3bcbd4651
-// (6) 0x1a71a8da3c00542a1c5adb9ad8e9b68493d30a2f4419daacdce4d1ca66470474
-// (7) 0x22911ba60acf21a828f6483cda5d3730e5d220145c91ddf93ec7fb6cccc202aa
-// (8) 0x5dc73e008db123cd1a2fbaf67d2712d912128498f82ec61d9af8a22a70de509f
-// (9) 0x244faf2038c87ff122252f8de931fe5a589ff89df4bdd8239d94b19bc14a7dc3
