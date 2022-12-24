a4bb23960283730b8b412f519a863c980e8eaccd
JongChan-Kim98 <whdcks9920@gmail.com>
2022-12-24 12:51:34 +0900

    민팅페이지

--- front/react/src/components/Page/Minting.js
+++ front/react/src/components/Page/Minting.js
@@ -8,6 +8,8 @@ const Minting = () => {
       <SideNav />
       <div className={classes['padding-left360']}>
         <div>
+          <div>트랜잭션당 민팅 가능 수량 : 1</div>
+          <div>{0}/{30}</div>
           <button>mint</button>
         </div>
       </div>
